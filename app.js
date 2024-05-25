2c5c4b12d47b767bc05ac8adb0d5dff53b444923
ExtraSadCheese <sadcheesealt@gmail.com>
2024-05-25 14:55:24 +0200

    Backend afgemaakt
    Email validator gefixt. Resend toegepast om emails te sturen. Subscribe/unsubscribe/verification functioneel met de frontend.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient('https://yzlkvjdkppkodpneavwl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6bGt2amRrcHBrb2RwbmVhdndsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTQ0ODA5NiwiZXhwIjoyMDI3MDI0MDk2fQ.pvn5m06axtpfKwAPuX4DpB2EtV1X2bJVCw1cVOaxMxQ')
 const databaseName = 'subscriptions';
-const frontEndUrl = 'http://localhost:5173'
+// const frontEndUrl = 'http://localhost:5173'
+const frontEndUrl = 'https://ayw-groep-02.netlify.app'
 
 const { Resend } = require('resend');
 const resend = new Resend('re_M3dCKe6R_QKaoPs2RjKK19yDYjtA2fKSQ');
@@ -62,10 +63,27 @@ async function getUserByEmail(email) {
 
 // Function to remove user from supabase by token
 async function deleteUser(verification_token) {
-  const { data, error } = await supabase
-        .from(databaseName)
-        .delete()
-        .eq('verification_token', verification_token);
+  try {
+      const { data, error } = await supabase
+          .from(databaseName)
+          .delete()
+          .eq('verification_token', verification_token);
+
+      if (error) {
+          throw error;
+      }
+
+      // Check if no rows were deleted
+      if (data && data.length === 0) {
+          throw new Error('No rows deleted');
+      }
+
+      console.log('User deleted successfully');
+      return { success: true };
+  } catch (error) {
+      console.error('Error deleting user:', error.message);
+      return { success: false, error: error.message };
+  }
 }
 
 // Function to define mailoptions  defineMailOptions
@@ -111,24 +129,22 @@ app.post('/api/new', async (req, res) => {
           const { data, error } = await supabase
             .from(databaseName)
             .insert(dataToSend);
-          
           if (error) {
             throw error;
           }  
-
           console.log('Data inserted successfully');
           res.status(200).json({ message: 'Data inserted successfully' });
         } catch (error) {
           console.error('Error inserting data to Supabase:', error.message);
           res.status(500).json({ error: 'Error inserting data to Supabase' });
         }
-
+        const request_type = "add";
         // Define mail content
         const emailContent = `
             <p>Thank you for signing up!</p>
             <p>Please click the following link to verify your email:</p>
-            <a href="${frontEndUrl}/api/verify?token=${verification_token}">Verify Email here</a>
-        `;
+            <a href="${frontEndUrl}/nieuwsbrief/bevestig?token=${verification_token}&type=${request_type}">Verify Email here</a>
+        `;  
 
         // Send verification mail
         sendEmail(userEmail, "Verify your email", emailContent);
@@ -141,44 +157,110 @@ app.post('/api/new', async (req, res) => {
 
 // UNSUBSCRIBE FORM SUBMIT
 app.delete('/api/unsubscribe', async (req, res) => {
-  const userEmail = req.body.email;
-  console.log("User email: "+userEmail)
+  const verification_token = req.query.token;
+  console.log("Received DELETE request with token:", verification_token);
 
-  getUserByEmail(userEmail)
-  .then(user => {
-    if (user) {
-      console.log('Email verified: ', user.confirmed)
-
-      if (user.confirmed === true) {
-        // Define mail content
-        const emailContent = `
-        <p>Click the following link to unsubscribe from our newsletter</p>
-        <a href="${frontEndUrl}/api/unsubscribe?token=${user.verification_token}">Verify Email here</a>
-        `;
-        // send email to user with token to unsubscribe
-        sendEmail(userEmail, "Confirm unsubscription", emailContent);
-
-      } else if (user.confirmed === false) {
-        // remove user from subscriptions without mail
-        deleteUser(user.verification_token);
-        console.log('user has been removed')
-
+  if (verification_token) {
+    try {
+      const deletedUser = await deleteUser(verification_token);
+      if (deletedUser) {
+        console.log("User deleted successfully:", deletedUser);
+        res.status(200).json({ message: 'User deleted successfully', data: deletedUser });
       } else {
-        console.log("\'confirmed\' is neither true nor false")
+        console.log("No user found with the provided token.");
+        res.status(404).json({ message: 'User not found' });
       }
-
-    } else {
-      console.log('User not found');
+    } catch (error) {
+      console.error('Error deleting user:', error.message);
+      res.status(500).json({ error: 'Error deleting user', message: error.message });
     }
-  })
-  .catch(error => {
+  } else {
+    const userEmail = req.body.email;
+    console.log("Received DELETE request with email:", userEmail);
+
+    getUserByEmail(userEmail)
+      .then(user => {
+        if (user) {
+          console.log('User found with email:', user);
+
+          if (user.confirmed === true) {
+            const request_type = "remove";
+            const emailContent = `
+              <p>Click the following link to unsubscribe from our newsletter</p>
+              <a href="${frontEndUrl}/nieuwsbrief/bevestig?token=${user.verification_token}&type=${request_type}">Verify Email here</a>
+            `;
+            sendEmail(userEmail, "Confirm unsubscription", emailContent);
+            console.log("Unsubscription email sent to user:", userEmail);
+            res.status(200).json({ message: 'Confirmation email sent for unsubscription' });
+          } else {
+            deleteUser(user.verification_token)
+              .then(deletedUser => {
+                console.log("User deleted without confirmation:", deletedUser);
+                res.status(200).json({ message: 'User deleted without confirmation', data: deletedUser });
+              })
+              .catch(error => {
+                console.error('Error deleting user:', error.message);
+                res.status(500).json({ error: 'Error deleting user', message: error.message });
+              });
+          }
+        } else {
+          console.log('User not found with email:', userEmail);
+          res.status(404).json({ message: 'User not found' });
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching user by email:', error);
+        res.status(500).json({ error: 'Error fetching user by email', message: error.message });
+      });
+  }
+});
+
+// VERIFY EMAIL (LINK)
+app.patch('/api/confirm', async (req, res) => {
+  const verification_token = req.query.token;
+
+  try {
+    // Check if the row exists
+    const { data: existingData, error: fetchError } = await supabase
+        .from(databaseName)
+        .select('confirmed')
+        .eq('verification_token', verification_token)
+        .single();
+    
+    if (fetchError) {
+      throw fetchError;
+    }
+
+    if (!existingData) {
+      return res.status(404).json({ error: 'Row not found', message: 'No row found with the provided token' });
+    }
+    
+    // If user is not confirmed, confirm them
+    if (existingData.confirmed === false) {
+      const { error: updateError } = await supabase
+          .from(databaseName)
+          .update({ confirmed: true })
+          .eq('verification_token', verification_token);
+
+      if (updateError) {
+        // Handle database update error
+        return res.status(500).json({ error: 'Error updating record', message: updateError.message });
+      }
+      
+      return res.status(200).json({ success: true, message: 'User confirmed successfully' });
+    } else {
+      console.log("This user is already confirmed");
+      return res.status(200).json({ message: 'User is already confirmed' });
+    }
+  } 
+  catch (error) {
     console.error('Error:', error);
-  });
+    res.status(500).json({ error: 'An unexpected error occurred', message: error.message });
+  }
 });
 
 
-
-// old stuff
+// Alle emails fetchen op render
 app.get('/api/subscriptions', (req, res) => {
   // Logic to fetch all e-mails
   supabase
@@ -195,48 +277,6 @@ app.get('/api/subscriptions', (req, res) => {
     });
 });
 
-app.get('/api/verify', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'landing.html'))
-  const verification_token = req.query.token;
-  
-  try {
-    // Check if the row exists and its 'confirmed' value is false
-    const { data: existingData, error: existingError } = await supabase
-        .from(databaseName)
-        .select('confirmed')
-        .eq('verification_token', verification_token)
-        .single();
-    
-    if (existingError) {
-        throw existingError;
-    }
-
-    if (!existingData) {
-        return res.status(404).json({ error: 'Row not found', message: 'No row found with the provided ID' });
-    }
-
-    if (!existingData.confirmed == true) {
-      const { error: updateError } = await supabase
-          .from(databaseName)
-          .update({ confirmed: true })
-          .eq('verification_token', verification_token);
-
-      if (updateError) {
-        // Handle database update error
-        return res.status(500).json({ error: 'Error updating record' });
-      }
-      
-      return res.status(200).json({ success: true });
-    } else {
-      console.log("Already confirmed: true");
-    }
-    
-  } 
-  catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An unexpected error occured' });
-  } 
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
